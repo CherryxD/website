ce770cc70239d8f9e2fdcbc20070d1ae7c2e9026
Cherry~ <54858196+CherryxD@users.noreply.github.com>
2021-09-27 14:48:51 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "10%";
+    document.getElementById("main").style.marginLeft = "10%";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "35px";
+  }
